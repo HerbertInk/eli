ed79a5421b5e49e0675c97f755d4ceba67b2ac89
davis <davis@localhost.localdomain>
2021-03-25 15:36:00 +0300

    Elian n

---
+++ d.py
@@ -0,0 +1,11 @@
+x1 = float(input('input x1: '))
+x2 = float(input('input x2: '))
+y1 = float(input('input y1: '))
+y2 = float(input('input y2: '))
+#d3 = (x2-x1)**2
+#d4 = (y2-y1)**2
+#d5 =d3 + d4
+#d6 = d5**0.5
+d = (((x2-x1)**2)+((y2-y1)**2))**.5
+#print(d6)
+print(d)
